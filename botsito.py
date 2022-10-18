feca490ca7bda2ffddd2964dabac01ed24dfb16a
Hector Fernandez <h4451477@gmail.com>
2022-10-17 20:26:09 -0400

    Bot v 1.1

--- botsito.py
+++ botsito.py
@@ -1,9 +1,10 @@
 from multiprocessing import connection
+from sys import flags
 import discord
 import requests
 
 import sqlite3
-connectionDB = sqlite3.connect("tutorial.db")
+connectionDB = sqlite3.connect("siu.db")
 cur = connectionDB.cursor()
 
 
@@ -38,23 +39,55 @@ async def on_message(message):
         await message.channel.send(f'El precio mas bajo es: {low}')
 
     if message.content.startswith('$info'):
-        pais = message.content.split(' ')[1]
-        info = requests.get(f'https://goweather.herokuapp.com/weather/{pais}')
-        response = info.json()
-        todayTemp = response['temperature']
-        info_pais = requests.get(f'https://restcountries.com/v3.1/name/{pais}')
+        if len(message.content) != 5:
+            pais = message.content.split(' ')[1]
+            info = requests.get(f'https://goweather.herokuapp.com/weather/{pais}')
+            response = info.json()
+            temperatura = response['temperature']
+            clima = response['description']
+            info_pais = requests.get(f'https://restcountries.com/v3.1/name/{pais}')
+            response_pais = info_pais.json()
+            name = response_pais[0]['name']['common']
+            capital = response_pais[0]['capital'][0]
+            population = response_pais[0]['population']
+            region = response_pais[0]['region']
+            flags = response_pais[0]['flags']['png']
+
+
+            await message.channel.send(f'<@{message.author.id}>, buscaste la información de {name}')
+            await message.channel.send(f' Su capital es : {capital}')
+            await message.channel.send(f'La temperatura es de {temperatura}')
+            await message.channel.send(f'El clima es : {clima}')
+            await message.channel.send(f'Tiene una población de {population} de habitantes')
+            await message.channel.send(f'Esta en la región de {region}')
+            await message.channel.send(f'{flags}')
+
+    else:
+
+
+        info_ip = requests.get(f'https://api.geoapify.com/v1/ipinfo?apiKey=b49dd1f965654ae591d926ec01b31d19')
+        response = info_ip.json()
+        country_ip = response['country']['names']['de']
+        temp = requests.get(f'https://goweather.herokuapp.com/weather/{country_ip}')
+        response_temp = temp.json()
+        temperatura = response_temp['temperature']
+        clima = response_temp['description']
+        info_pais = requests.get(f'https://restcountries.com/v3.1/name/{country_ip}')
         response_pais = info_pais.json()
         name = response_pais[0]['name']['common']
         capital = response_pais[0]['capital'][0]
         population = response_pais[0]['population']
         region = response_pais[0]['region']
-        bandera = response_pais[0]['flags']['png']
-        await message.channel.send(f'<@{message.author.id}>, buscaste la informacion de {name}')
+        flags = response_pais[0]['flags']['png']
+
+
+        await message.channel.send(f'<@{message.author.id}>, esta es la información de {name}')
         await message.channel.send(f' Su capital es: {capital}')
-        await message.channel.send(f'La temperatura es de {todayTemp}')
-        await message.channel.send(f'Tiene una poblacion de {population} habitantes')
-        await message.channel.send(f'Esta en la region de {region}')
-        await message.channel.send(f'{bandera}')
+        await message.channel.send(f'La temperatura es de {temperatura}')
+        await message.channel.send(f'El clima es : {clima}')
+        await message.channel.send(f'Tiene una población de {population} de habitantes')
+        await message.channel.send(f'Esta en la región de {region}')
+        await message.channel.send(f'{flags}')
         
 
     # if message.content.startswith('$info'):
@@ -64,13 +97,13 @@ async def on_message(message):
     #     name = response[0]['name']['common']
     #     await message.channel.send(f' {name}')
 
-        if message.content.startswith('!CrearUsuario'):
+        if message.content.startswith('$crear'):
             first_name = message.content.split(' ')[1]
             last_name = message.content.split(' ')[2]
             full_name = f'{first_name} {last_name}'
             cur.execute('INSERT INTO users (discord_id, name) VALUES (?, ?)', [message.author.id, full_name])
             connectionDB.commit()
-            await message.channel.send('Usuario Creado!')
+            await message.channel.send('usario creado')
 
     if message.content.startswith('!BorrarUsuario'):
         cur.execute('DELETE FROM users WHERE discord_id = ?', [message.author.id])
--- tables.py
+++ tables.py
@@ -1,8 +1,8 @@
 import sqlite3
-connectionDB = sqlite3.connect("tutorial.db")
+connectionDB = sqlite3.connect("siu.db")
 cur = connectionDB.cursor()
 
-cur.execute('''CREATE TABLE IF NOT EXISTS USERS(
+cur.execute('''CREATE TABLE IF NOT EXISTS users(
     discord_id TEXT PRIMARY KEY,
     name TEXT
 )''')
